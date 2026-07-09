7838dc5dc95363b6f24b07ff10b909d5fee4165a
Jaydon Pearce <pearcejaydon@gmail.com>
2026-07-09 20:47:06 +1000

    setup app with db connection

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
